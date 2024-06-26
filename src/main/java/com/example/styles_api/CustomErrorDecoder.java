fb92b4e7373d706f42233df26f099f784bf87bac
Gyokhan <gyokhan28@gmail.com>
2024-06-26 20:10:44 +0300

    fixed high issue

--- src/main/java/com/example/styles_api/CustomErrorDecoder.java
+++ src/main/java/com/example/styles_api/CustomErrorDecoder.java
@@ -13,7 +13,8 @@ public class CustomErrorDecoder implements ErrorDecoder {
             case HttpStatus.SC_UNAUTHORIZED: return new OpenApiResourceNotFoundException("You might want to authorize first");
             case HttpStatus.SC_INTERNAL_SERVER_ERROR: return new OpenApiResourceNotFoundException("server error????");
             case HttpStatus.SC_NOT_FOUND: return new OpenApiResourceNotFoundException("This is not the page you are looking for");
+            default:
+                return new OpenApiResourceNotFoundException("An unexpected error occurred: " + status);
         }
-        return null;
     }
 }
